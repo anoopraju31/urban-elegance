f153a70501de5b1c47226435a1eff171e130ee14
anoopraju31 <anoopraju31@gmail.com>
2024-06-12 17:24:48 +0530

    Login Page styled

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,89 @@
+'use client'
+
 import { FC } from 'react'
-import SignInWithGoogle from '@/components/SignInWithGoogle'
+import { signIn, useSession } from 'next-auth/react'
+import { redirect } from 'next/navigation'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 
-type Props = {}
+const Page: FC = () => {
+	const { data: session } = useSession()
 
-const page: FC<Props> = (props) => {
+	if (session && session.user) redirect('/')
+
 	return (
-		<div>
-			<h1> login page </h1>
-			<SignInWithGoogle />
+		<div className='w-full bg-gray-900 lg:grid min-h-[var(--hero-image-height-sm)] lg:grid-cols-2 md:min-h-[var(--hero-image-height-lg)]'>
+			<div className='flex items-center justify-center py-12'>
+				<div className='mx-auto grid w-[350px] gap-6'>
+					<div className='grid gap-2 text-center'>
+						<h1 className='text-3xl font-bold'>Login</h1>
+						<p className='text-balance text-muted-foreground'>
+							Enter your email below to login to your account
+						</p>
+					</div>
+					<div className='grid gap-4'>
+						<div className='grid gap-2'>
+							<Label htmlFor='email' className='sr-only'>
+								Email
+							</Label>
+							<Input
+								id='email'
+								type='email'
+								placeholder='m@example.com'
+								required
+							/>
+						</div>
+						<div className='grid gap-2'>
+							<div className='flex items-center'>
+								<Label htmlFor='password' className='sr-only'>
+									Password
+								</Label>
+							</div>
+							<Input
+								id='password'
+								type='password'
+								placeholder='password'
+								required
+							/>
+						</div>
+						<Button type='submit' className='w-full'>
+							Login
+						</Button>
+
+						<Link
+							href='/forgot-password'
+							className='inline-block text-center text-sm underline'>
+							Forgot your password?
+						</Link>
+						<Button
+							onClick={() => signIn('google')}
+							variant='outline'
+							className='w-full bg-red-500'>
+							Login with Google
+						</Button>
+					</div>
+					<div className='mt-4 text-center text-sm'>
+						Don&apos;t have an account?{' '}
+						<Link href='#' className='underline'>
+							Sign up
+						</Link>
+					</div>
+				</div>
+			</div>
+			<div className='hidden bg-muted lg:block'>
+				<Image
+					src='/login.jpg'
+					alt='Image'
+					width='1920'
+					height='1080'
+					className='h-full w-full object-cover'
+				/>
+			</div>
 		</div>
 	)
 }
 
-export default page
+export default Page
