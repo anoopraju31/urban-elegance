81d2154ac829b745037ddaacf9211a87d340cc2f
anoopraju31 <anoopraju31@gmail.com>
2024-06-12 15:41:33 +0530

    Product Image Carousel - initial

---
+++ components/ProductImageCarousel.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+type Props = {}
+
+const ProductImageCarousel: FC<Props> = (props) => {
+	return <div>ProductImageCarousel</div>
+}
+
+export default ProductImageCarousel
