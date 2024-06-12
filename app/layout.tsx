04ebbe1f23855443282d2ea029b58bc125241b7f
anoopraju31 <anoopraju31@gmail.com>
2024-06-12 09:18:47 +0530

    Session Provider added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import './globals.css'
+import AuthProvider from '@/components/AuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,9 @@ type Props = {
 const RootLayout: FC<Readonly<Props>> = ({ children }) => {
 	return (
 		<html lang='en'>
-			<body className={inter.className}> {children} </body>
+			<body className={inter.className}>
+				<AuthProvider> {children} </AuthProvider>
+			</body>
 		</html>
 	)
 }
