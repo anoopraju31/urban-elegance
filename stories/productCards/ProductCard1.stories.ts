42216758ae820657fc93b8e8e1d336f79b5336f9
anoopraju31 <anoopraju31@gmail.com>
2024-06-14 00:53:38 +0530

    Product Card 1 refactor

--- stories/productCards/ProductCard1.stories.ts
+++ stories/productCards/ProductCard1.stories.ts
@@ -16,7 +16,65 @@ export const Base: Story = {
 			'https://www.beyoung.in/api/cache/catalog/products/new_chinos_update_image_23_9_2022/wood_brown_chinos_for_men_base_05_06_2023_400x533.jpg',
 			'https://www.beyoung.in/api/cache/catalog/products/new_chinos_update_image_23_9_2022/camel_beige_chinos_for_men_full_view_25_1_2023_400x533.jpg',
 		],
-		isHoverableImg: false,
+		isHoverableImg: true,
+		title: 'Black Mens Chino Pants',
+		category: 'Chino Pants',
+		discountedPrice: 998,
+		price: 2399,
+		discountPercent: 58,
+		titleColor: 'text-red-500',
+		categoryColor: 'text-purple-300',
+		priceColor: 'text-red-600',
+		discountedPriceColor: 'text-slate-300',
+		discountedPercentColor: 'text-blue-300',
+	},
+}
+
+export const HoverableCard: Story = {
+	args: {
+		img: [
+			'https://www.beyoung.in/api/cache/catalog/products/new_chinos_update_image_23_9_2022/wood_brown_chinos_for_men_base_05_06_2023_400x533.jpg',
+			'https://www.beyoung.in/api/cache/catalog/products/new_chinos_update_image_23_9_2022/camel_beige_chinos_for_men_full_view_25_1_2023_400x533.jpg',
+		],
+		isHoverableImg: true,
+		badge: 'Top Rated',
+		title: 'Black Mens Chino Pants',
+		category: 'Chino Pants',
+		discountedPrice: 998,
+		price: 2399,
+		discountPercent: 58,
+		titleColor: 'text-red-500',
+		categoryColor: 'text-purple-300',
+		priceColor: 'text-red-600',
+		discountedPriceColor: 'text-slate-300',
+		discountedPercentColor: 'text-blue-300',
+	},
+}
+
+export const NonHoverableCard: Story = {
+	args: {
+		img: [
+			'https://www.beyoung.in/api/cache/catalog/products/new_chinos_update_image_23_9_2022/wood_brown_chinos_for_men_base_05_06_2023_400x533.jpg',
+		],
+		title: 'Black Mens Chino Pants',
+		category: 'Chino Pants',
+		discountedPrice: 998,
+		price: 2399,
+		discountPercent: 58,
+		titleColor: 'text-red-500',
+		categoryColor: 'text-purple-300',
+		priceColor: 'text-red-600',
+		discountedPriceColor: 'text-slate-300',
+		discountedPercentColor: 'text-blue-300',
+	},
+}
+
+export const CardWithBadge: Story = {
+	args: {
+		img: [
+			'https://www.beyoung.in/api/cache/catalog/products/new_chinos_update_image_23_9_2022/wood_brown_chinos_for_men_base_05_06_2023_400x533.jpg',
+		],
+		badge: 'Top Rated',
 		title: 'Black Mens Chino Pants',
 		category: 'Chino Pants',
 		discountedPrice: 998,
--- stories/productCards/ProductCard1.tsx
+++ stories/productCards/ProductCard1.tsx
@@ -2,44 +2,55 @@ import Image from 'next/image'
 import { FC } from 'react'
 import { FaHeart } from 'react-icons/fa6'
 
-type Props = {
+interface ProductCardBaseProps {
 	img: string[]
 	isHoverableImg?: boolean
+	badge?: 'New' | 'Sale' | 'Top Rated' | 'Best Seller'
 	title: string
-	category: string
-	price: number
-	discountedPrice?: number
-	discountPercent?: number
 	titleColor?: string
+	category: string
 	categoryColor?: string
+	price: number
 	priceColor?: string
+}
+
+interface DiscountedProductCardProps extends ProductCardBaseProps {
+	discountedPrice: number
+	discountPercent: number
 	discountedPriceColor?: string
 	discountedPercentColor?: string
 }
 
-const ProductCard1: FC<Props> = ({
-	img,
-	isHoverableImg,
-	title,
-	category,
-	price,
-	discountPercent,
-	discountedPrice,
-	titleColor = 'text-gray-700',
-	categoryColor = 'text-gray-400',
-	discountedPriceColor = 'text-gray-900',
-	priceColor = 'text-gray-300',
-	discountedPercentColor = 'text-green-500',
-}) => {
+type ProductCardProps = ProductCardBaseProps | DiscountedProductCardProps
+
+const ProductCard1: FC<ProductCardProps> = (props) => {
+	const {
+		img,
+		isHoverableImg = false,
+		badge,
+		title,
+		category,
+		price,
+		titleColor = 'text-gray-700',
+		categoryColor = 'text-gray-400',
+		priceColor = 'text-gray-300',
+	} = props
+	const {
+		discountPercent,
+		discountedPercentColor = 'text-green-500',
+		discountedPrice,
+		discountedPriceColor = 'text-gray-900',
+	} = props as DiscountedProductCardProps
+
 	return (
 		<div className='max-w-56 group'>
-			<div className='relative'>
+			<div className='relative rounded-md overflow-hidden'>
 				<Image
 					src={img[0]}
 					alt={title}
 					width={300}
 					height={400}
-					className={`w-full object-cover rounded-md ${isHoverableImg && 'group-hover:hidden'} transition-all duration-500 ease-in-out`}
+					className={`w-full object-cover ${isHoverableImg && 'group-hover:hidden'} transition-all duration-500 ease-in-out`}
 				/>
 
 				{isHoverableImg && (
@@ -48,13 +59,19 @@ const ProductCard1: FC<Props> = ({
 						alt={title}
 						width={300}
 						height={400}
-						className='hidden w-full object-cover rounded-md group-hover:inline-block transition-all duration-500 ease-in-out'
+						className='hidden w-full object-cover group-hover:inline-block transition-all duration-500 ease-in-out'
 					/>
 				)}
 
 				<div className='absolute top-4 right-4 w-8 h-8 rounded-full bg-white text-red-500 flex justify-center items-center'>
 					<FaHeart />
 				</div>
+
+				{badge && (
+					<div className='absolute top-0 left-0 px-2 py-1 text-xs text-white bg-red-500 rounded-br-md'>
+						{badge}
+					</div>
+				)}
 			</div>
 
 			<div className='px-1 py-2'>
@@ -66,11 +83,21 @@ const ProductCard1: FC<Props> = ({
 				</p>
 
 				<div className='mt-1 flex items-center gap-1 text-base'>
-					<div className={`${discountedPriceColor} font-medium`}>
-						${discountedPrice}
+					<div className={`${priceColor} font-medium`}>
+						${discountedPrice || price}
 					</div>
-					<div className={`${priceColor} line-through`}> ${price} </div>
-					<div className={discountedPercentColor}>({discountPercent}% off)</div>
+
+					{discountedPrice && (
+						<div className={`${discountedPriceColor} line-through`}>
+							${price}
+						</div>
+					)}
+
+					{discountPercent && (
+						<div className={discountedPercentColor}>
+							({discountPercent}% off)
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
