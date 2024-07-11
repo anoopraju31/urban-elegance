d4fe75c4b725bbae33ce5ce06a466c05340c506f
anoopraju31 <anoopraju31@gmail.com>
2024-07-11 20:30:47 +0530

    Home Page refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from '@/components/HeroSection'
 import ProductsSection from '@/components/ProductsSection'
 import { FC } from 'react'
+import CategoryItem from './products/category/_components/categoryItem'
 
 type Props = {}
 
@@ -8,7 +9,27 @@ const page: FC<Props> = (props) => {
 	return (
 		<main className='min-h-screen text-gray-600 dark:text-gray-400 bg-white dark:bg-gray-900 body-font overflow-hidden'>
 			<HeroSection />
-			<ProductsSection />
+			<div className='max-w-screen-xl w-full mx-auto my-16 p-10 rounded-lg bg-gray-800'>
+				<h2 className='text-2xl text-center mb-5'> Shop by Category </h2>
+				<div className='flex items-center gap-6 overflow-auto no-scrollbar'>
+					<CategoryItem />
+					<CategoryItem />
+					<CategoryItem />
+					<CategoryItem />
+					<CategoryItem />
+					<CategoryItem />
+					<CategoryItem />
+					<CategoryItem />
+					<CategoryItem />
+					<CategoryItem />
+					<CategoryItem />
+					<CategoryItem />
+					<CategoryItem />
+					<CategoryItem />
+					<CategoryItem />
+				</div>
+			</div>
+			{/* <ProductsSection /> */}
 		</main>
 	)
 }
--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
 				protocol: 'https',
 				hostname: 'flowbite.s3.amazonaws.com',
 			},
+			{
+				protocol: 'https',
+				hostname: 'coralperfumes.cloud6.ae',
+			},
 		],
 	},
 }
